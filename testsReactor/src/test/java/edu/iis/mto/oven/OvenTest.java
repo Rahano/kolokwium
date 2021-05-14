7a86b5df48fe7ebaec444935cb133e13dc7d999f
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-05-14 17:13:49 +0200

    Refactor tests and implement test for bakingProgram containing multiple programStages

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -23,9 +23,10 @@ class OvenTest {
     HeatingModule heatingModule;
 
     private Oven oven;
-    private HeatingSettings heatingSettings;
     private BakingProgram bakingProgram;
     private ProgramStage programStage1;
+    private ProgramStage programStage2;
+    private ProgramStage programStage3;
     private List<ProgramStage> stageList;
 
     @BeforeEach
@@ -36,38 +37,58 @@ class OvenTest {
     @Test
     void ovenWithBakingProgramContainingOneProgramStageShouldCallHeatingModuleOnceToActivateHeater() {
         programStage1 = ProgramStage.builder().withHeat(HeatType.HEATER).withStageTime(5).withTargetTemp(180).build();
-        heatingSettings = HeatingSettings.builder().withTargetTemp(180).withTimeInMinutes(5).build();
         stageList = List.of(programStage1);
         bakingProgram = BakingProgram.builder().withInitialTemp(180).withStages(stageList).build();
         oven.start(bakingProgram);
-        Mockito.verify(heatingModule, Mockito.times(1)).heater(heatingSettings);
+
+        Mockito.verify(heatingModule, Mockito.times(1)).heater(settings(programStage1));
     }
 
     @Test
     void ovenWithBakingProgramContainingOneProgramStageShouldCallFanToTurnOnOnceAndTurnOffOnceANdHeatingModuleOnceToActivateThermalCircuit() throws HeatingException {
         programStage1 = ProgramStage.builder().withHeat(HeatType.THERMO_CIRCULATION).withStageTime(10).withTargetTemp(200).build();
-        heatingSettings = HeatingSettings.builder().withTargetTemp(200).withTimeInMinutes(10).build();
         stageList = List.of(programStage1);
         bakingProgram = BakingProgram.builder().withInitialTemp(200).withStages(stageList).build();
         oven.start(bakingProgram);
+
         Mockito.verify(fan, Mockito.times(1)).on();
         Mockito.verify(fan, Mockito.times(1)).off();
-        Mockito.verify(heatingModule, Mockito.times(1)).termalCircuit(heatingSettings);
+        Mockito.verify(heatingModule, Mockito.times(1)).termalCircuit(settings(programStage1));
     }
 
     @Test
     void ovenWithBakingProgramContainingOneProgramStageShouldCallFanToTurnOnOnceAndTurnOffOnceANdHeatingModuleOnceToActivateThermalCircuitShouldThrowOvenException() throws HeatingException {
         programStage1 = ProgramStage.builder().withHeat(HeatType.THERMO_CIRCULATION).withStageTime(10).withTargetTemp(100).build();
-        heatingSettings = HeatingSettings.builder().withTargetTemp(100).withTimeInMinutes(10).build();
         stageList = List.of(programStage1);
         bakingProgram = BakingProgram.builder().withInitialTemp(200).withStages(stageList).build();
-        Mockito.doThrow(HeatingException.class).when(heatingModule).termalCircuit(heatingSettings);
+        Mockito.doThrow(HeatingException.class).when(heatingModule).termalCircuit(settings(programStage1));
 
         Assertions.assertThrows(OvenException.class, () -> {
             oven.start(bakingProgram);
         });
         Mockito.verify(fan, Mockito.times(1)).on();
         Mockito.verify(fan, Mockito.times(1)).off();
-        Mockito.verify(heatingModule, Mockito.times(1)).termalCircuit(heatingSettings);
+        Mockito.verify(heatingModule, Mockito.times(1)).termalCircuit(settings(programStage1));
+    }
+
+    @Test
+    void ovenWithBakingProgramContainingThreeProgramStageShouldCallHeatingModuleThreeTimesToActivateHeaterTwoTimesAndGrillOnce() {
+        programStage1 = ProgramStage.builder().withHeat(HeatType.HEATER).withStageTime(5).withTargetTemp(180).build();
+        programStage2 = ProgramStage.builder().withHeat(HeatType.GRILL).withStageTime(5).withTargetTemp(200).build();
+        programStage3 = ProgramStage.builder().withHeat(HeatType.HEATER).withStageTime(10).withTargetTemp(180).build();
+        stageList = List.of(programStage1, programStage2, programStage3);
+        bakingProgram = BakingProgram.builder().withInitialTemp(180).withStages(stageList).build();
+        oven.start(bakingProgram);
+
+        Mockito.verify(heatingModule, Mockito.times(1)).heater(settings(programStage1));
+        Mockito.verify(heatingModule, Mockito.times(1)).grill(settings(programStage2));
+        Mockito.verify(heatingModule, Mockito.times(1)).heater(settings(programStage3));
+    }
+
+    private HeatingSettings settings(ProgramStage stage) {
+        return HeatingSettings.builder()
+                              .withTargetTemp(stage.getTargetTemp())
+                              .withTimeInMinutes(stage.getStageTime())
+                              .build();
     }
 }
